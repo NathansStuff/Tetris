16ed4d43f31f260982ddb771f35fe1fb6ae52dc9
Nathanstuff <n.odonnell@hotmail.com>
2020-12-15 11:14:48 +1100

    static drawing 1 tetromino

---
+++ script.js
@@ -0,0 +1,54 @@
+const grid = document.querySelector('grid')
+const squares = Array.from(document.querySelectorAll('.grid div'))
+const width = 10
+//The Tetrominoes
+const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+let currentTetromino = theTetrominoes[0][0]
+
+let currentPostition = 4
+
+
+function draw() {
+    console.log(squares)
+    currentTetromino.forEach(index => {
+        squares[currentPostition + index].classList.add('active')
+    })
+}
+
+draw()
